7e033ff4baff3dc13b4c2792d09333c7eaff59c5
Martin <martin.limberger@gmx.net>
2019-07-26 14:23:34 +0200

    test

--- src/environment/src/testGame1.js
+++ src/environment/src/testGame1.js
@@ -112,6 +112,8 @@ function testLoop(Factory, Executor, seedValue, type, useOptimization, mi, algoT
                             const startDate = Date.now();
                             const generator = Generator.create(seedValue);
 
+                            console.log(`${algoType};${type};${parameters.populationSizeArray[populationIndex]};${parameters.generationCountArray[generationCountIndex]};${parameters.playerCount};${parameters.strategyCount};${parameters.selectionPressureArray[selectionPressureIndex]};${parameters.mutationRateArray[mutationIndex]};${selectionFunctionArray[selectionFunctionIndex].name};${replacementFunctionArray[replacementFunctionIndex].name};${useOptimization};`);
+
                             for (let p = 0; p < parameters.maxRounds; p++) {
                                 roundIndex = p;
                                 const dynSeedValue = generator.range(10000);
