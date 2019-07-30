797ea60639bf76ad003306394e21075810c8f0da
Martin Limberger <martin.limberger@gmx.net>
2019-07-30 20:00:47 +0200

    start game 6 changes

--- src/environment/src/testGame6.js
+++ src/environment/src/testGame6.js
@@ -1,12 +1,12 @@
 
+
 import Generator from 'random-seed';
 import fs from 'fs';
-import ExecutorES6 from './algorithms/Game6/ExecutorES';
-import ExecutorGA6 from './algorithms/Game6/ExecutorGA';
-import CandidateFactory6 from './algorithms/Game6/CandidateFactory';
+import { proportionalSelection, randomSelection, tournamentSelection, completeReplacement, randomReplacement, elitismReplacement } from './algorithms/util';
 
 
-const roundArray = [1,10,25,50];
+let parameters = {};
+
 
 let resultX = 0;
 let resultY = 0;
@@ -41,29 +41,17 @@ function log() {
     output += '\n';
 }
 
-function newMessage(gen, type, msg) {
-    if (type === 'fin') {
-        for (let i = 0; i < playerCount; i++) {
-            resultX += Math.abs(lastResultsX[i] - (xMax/2));
-            resultY += Math.abs(lastResultsY[i] - (yMax/2));
-            lastResultsX[i] = 0;
-            lastResultsY[i] = 0;
-        }
-    }
-  }
 
-function newMessage2(gen, type, msg) {
+function newMessage(gen, type, msg) {
     if (type === 'fin') {
         let avg = 0;
 
+        for (let j = 0; j < playerCount; j++) {
+            avg += lastResultsFitness[j];
+        }
+        avg = avg / playerCount;
+
         for (let i = 0; i < playerCount; i++) {
-            if (i === 0) {
-                for (let j = 0; j < playerCount; j++) {
-                    avg += lastResultsFitness[j];
-                    // log(lastResultsFitness[j]);
-                }
-                avg = avg / playerCount;
-            }
             resultX += Math.abs(lastResultsFitness[i] - avg);
             lastResultsFitness[i] = 0;
         }
@@ -77,11 +65,107 @@ function newGameState(data) {
 }
 
 
-function testGame3Execution(type='NE') {
+function testLoop(Factory, Executor, seedValue, type, useOptimization, mi, algoType) {
+    modeIndex = mi;
+    resultArray[modeIndex] = new Array(parameters.populationSizeArray.length);
+    const timeout = '0';
+
+    for (let i = 0; i < parameters.populationSizeArray.length; i++) {
+        populationIndex = i;
+        resultArray[modeIndex][populationIndex] = new Array(parameters.generationCountArray.length);
+        // log('Population: ', populationSizeArray[populationIndex]);
+        
+        for (let j = 0; j < parameters.generationCountArray.length; j++) {
+            generationCountIndex = j;
+            resultArray[modeIndex][populationIndex][generationCountIndex] = new Array(parameters.mutationRateArray.length);
+            // log('GenerationCount: ', generationCountArray[generationCountIndex]);
+
+            for (let k = 0; k < parameters.mutationRateArray.length; k++) {
+                mutationIndex = k;
+                resultArray[modeIndex][populationIndex][generationCountIndex][mutationIndex] = new Array(selectionFunctionArray.length);
+                // log('Mutation: ', mutationRateArray[mutationIndex]);
+
+                for (let l = 0; l < selectionFunctionArray.length; l++) {
+                    selectionFunctionIndex = l;
+                    resultArray[modeIndex][populationIndex][generationCountIndex][mutationIndex][selectionFunctionIndex] = new Array(replacementFunctionArray.length);
+                    // log('SelectionFunction: ', selectionFunctionArray[selectionFunctionIndex].name);
+
+                    for (let m = 0; m < replacementFunctionArray.length; m++) {
+                        replacementFunctionIndex = m;
+                        resultArray[modeIndex][populationIndex][generationCountIndex][mutationIndex][selectionFunctionIndex][replacementFunctionIndex] = new Array(parameters.selectionPressureArray.length);
+                        // log('ReplacementFunction: ', replacementFunctionArray[replacementFunctionIndex].name);
+
+                        for (let n = 0; n < parameters.selectionPressureArray.length; n++) {
+                            selectionPressureIndex = n;
+                            resultArray[modeIndex][populationIndex][generationCountIndex][mutationIndex][selectionFunctionIndex][replacementFunctionIndex][selectionPressureIndex] = new Array(parameters.maxRounds);
+                            bestSetting[modeIndex] = { populationIndex: -1 };
+
+                            result = 0;
+                            const startDate = Date.now();
+                            const generator = Generator.create(seedValue);
+
+                            console.log(`${algoType};${type};${parameters.populationSizeArray[populationIndex]};${parameters.generationCountArray[generationCountIndex]};${parameters.playerCount};${parameters.strategyCount};${parameters.selectionPressureArray[selectionPressureIndex]};${parameters.mutationRateArray[mutationIndex]};${selectionFunctionArray[selectionFunctionIndex].name};${replacementFunctionArray[replacementFunctionIndex].name};${useOptimization};`);
+
+                            for (let p = 0; p < parameters.maxRounds; p++) {
+                                roundIndex = p;
+                                const dynSeedValue = generator.range(10000);
+                                const factory = new Factory(parameters.playerCount || parameters.treeDepth, parameters.strategyCount, dynSeedValue, type);
+                                const executor = new Executor(parameters.generationCountArray[generationCountIndex], dynSeedValue, parameters.populationSizeArray[populationIndex], timeout, parameters.selectionPressureArray[selectionPressureIndex], parameters.mutationRateArray[mutationIndex], factory, newGameState, newMessage, selectionFunctionArray[selectionFunctionIndex], replacementFunctionArray[replacementFunctionIndex], useOptimization);
+                                executor.start();
+                            }
+
+                            const time = Date.now() - startDate;
+
+                            const newOutputLine = `${algoType};${type};${parameters.populationSizeArray[populationIndex]};${parameters.generationCountArray[generationCountIndex]};${parameters.playerCount};${parameters.strategyCount};${parameters.selectionPressureArray[selectionPressureIndex]};${parameters.mutationRateArray[mutationIndex]};${selectionFunctionArray[selectionFunctionIndex].name};${replacementFunctionArray[replacementFunctionIndex].name};${useOptimization};${time};${result}\n`;
+                            console.log('Line added', lineCount);
+                            lineCount++;
+                            fs.appendFile("results/game1.csv", newOutputLine, function(err) {
+                                if(err) {
+                                    return log(err);
+                                }
+                            }); 
+
+                            if (result > bestSetting[modeIndex].result || bestSetting[modeIndex].populationIndex === -1) {
+                                bestSetting[modeIndex].time = time;
+                                bestSetting[modeIndex].populationIndex = populationIndex;
+                                bestSetting[modeIndex].generationCountIndex = generationCountIndex;
+                                bestSetting[modeIndex].mutationIndex = mutationIndex;
+                                bestSetting[modeIndex].selectionFunctionIndex = selectionFunctionIndex;
+                                bestSetting[modeIndex].replacementFunctionIndex = replacementFunctionIndex;
+                                bestSetting[modeIndex].selectionPressureIndex = selectionPressureIndex;
+                                bestSetting[modeIndex].result = result;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    log('Population:', parameters.populationSizeArray[bestSetting[modeIndex].populationIndex]);
+    log('Generations:', parameters.generationCountArray[bestSetting[modeIndex].generationCountIndex]);
+    log('Mutation:', parameters.mutationRateArray[bestSetting[modeIndex].mutationIndex]);
+    log('Selection Function:', selectionFunctionArray[bestSetting[modeIndex].selectionFunctionIndex].name);
+    log('Replacement Function:', replacementFunctionArray[bestSetting[modeIndex].replacementFunctionIndex].name);
+    log('Result:', bestSetting[modeIndex].result);
+    log('Time:', bestSetting[modeIndex].time);
+}
+
+function testGame6Execution(type='NE') {
     let executor;
-    let startDate;
     let factory;
-    let generator;
+
+    const parametersFile = fs.readFileSync('game6.json');
+    parameters = JSON.parse(parametersFile);
+
+    const seedValue = Math.random() * 10000;
+    resultArray = new Array(5);
+
+    const header = 'Algo;Type;Population;Generations;Players;Strategys;SelectionPressure;MutationRate;SelectionFunction;ReplacementFunction;Optimized;Time;Result\n'
+    fs.writeFile("results/game6.csv", header, function(err) {
+        if(err) {
+            return log(err);
+        }
+    }); 
 
 
     const seedValue = Math.random() * 10000;
@@ -122,7 +206,7 @@ function testGame3Execution(type='NE') {
             roundCounter = j;
             const dynSeedValue = generator.range(10000);
             factory = new CandidateFactory6(playerCount,xMax, yMax, dynSeedValue);
-            executor = new ExecutorGA6(generationCount, dynSeedValue, populationSize, timeout, mutationRate, factory, newGameState, newMessage2);
+            executor = new ExecutorGA6(generationCount, dynSeedValue, populationSize, timeout, mutationRate, factory, newGameState, newMessage);
             executor.start();
         }
         log('Results: ', resultX, ', ', resultY);
@@ -149,7 +233,7 @@ function testGame3Execution(type='NE') {
             roundCounter = j;
             const dynSeedValue = generator.range(10000);
             factory = new CandidateFactory6(playerCount, xMax, yMax, dynSeedValue);
-            executor = new ExecutorES6(generationCount, dynSeedValue, populationSize, timeout, mutationRate*2, factory, newGameState, newMessage2);
+            executor = new ExecutorES6(generationCount, dynSeedValue, populationSize, timeout, mutationRate*2, factory, newGameState, newMessage);
             executor.start();
         }
         log('Results: ', resultX, ', ', resultY);
@@ -164,4 +248,4 @@ function testGame3Execution(type='NE') {
     }); 
 }
 
-export default testGame3Execution;
+export default testGame6Execution;
